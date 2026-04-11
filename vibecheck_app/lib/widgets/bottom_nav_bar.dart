7215e8385514f9fffe82e734b57327a31f76138c
Aiiiii <aiko.amonrat@gmail.com>
2026-04-11 20:57:59 +0700

    Add files via upload

--- vibecheck_app/lib/widgets/bottom_nav_bar.dart
+++ vibecheck_app/lib/widgets/bottom_nav_bar.dart
@@ -1,31 +1,45 @@
-// lib/screens/widgets/bottom_nav_bar.dart
 import 'package:flutter/material.dart';
+import '../screens/home_screen.dart';
+import '../screens/task_screen.dart';
+import '../screens/timer_screen.dart';
+import '../screens/emotion_screen.dart';
+import '../screens/settings_screen.dart';
 
-class BottomNavBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+  
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
 
-  const BottomNavBar({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const TaskScreen(),
+    const TimerScreen(),
+    const EmotionScreen(),
+    const SettingsScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: const Color(0xFF7B5EA7),
-      unselectedItemColor: Colors.grey,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.checklist), label: 'Tasks'),
-        BottomNavigationBarItem(icon: Icon(Icons.timer), label: 'Timer'),
-        BottomNavigationBarItem(icon: Icon(Icons.emoji_emotions), label: 'Emotion'),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-      ],
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF7B5EA7),
+        unselectedItemColor: Colors.grey,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), activeIcon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.list_alt_outlined), activeIcon: Icon(Icons.list_alt), label: 'Tasks'),
+          BottomNavigationBarItem(icon: Icon(Icons.timer_outlined), activeIcon: Icon(Icons.timer), label: 'Time'),
+          BottomNavigationBarItem(icon: Icon(Icons.emoji_emotions_outlined), activeIcon: Icon(Icons.emoji_emotions), label: 'Emotion'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), activeIcon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
     );
   }
 }
